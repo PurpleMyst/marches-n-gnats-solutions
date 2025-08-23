a8531530c1f6ea46e04b905afb8452a8cc242488
PurpleMyst <PurpleMyst@users.noreply.github.com>
2025-08-23 16:32:29 +0200

    perf(14/mirrored): optimize input sequence so now it''s actually faster than base

--- 14_binary_to_decimal/mirrored_solution.py
+++ 14_binary_to_decimal/mirrored_solution.py
@@ -5,35 +5,32 @@ from string import digits
 # number. For example, if the input tape is `1010`, your output tape should be `10`.
 
 BITS = set("01")
-OUT = "@"  # marker for the output section
+SOO = "@"  # start of output
+EOI = "#"  # end of input
+
 
 def main() -> None:
     with Program() as p:
-        for b in BITS:
-            p("INIT", b, "INIT2", b, "L")
-        p("INIT2", "_", "INIT3", OUT, "L")
-        p("INIT3", "_", "INIT4", "0", "R")
-        p.find("INIT4", "_", {*BITS, OUT}, "R", "INIT5", "E", "L")
-        p.find("INIT5", OUT, BITS, "L", "GET_BIT", OUT, "R")
-        p.find("TO_INPUT", OUT, set(digits), "R", "GET_BIT", OUT, "R")
+        # Initialize the tape with markers
+        p.find("INIT", "_", BITS, "R", "INIT2", EOI, "L")
+        p.find("INIT2", "_", BITS, "L", "INIT3", SOO, "L")
+        p("INIT3", "_", "TO_INPUT", "0", "R")
 
+        # Main loop: read bits and double the current output, incrementing if the bit is 1
+        p.find("TO_INPUT", SOO, set(digits), "R", "GET_BIT", SOO, "R")
         p.ignore("GET_BIT", "_", "R")
         p("GET_BIT", "0", "DOUBLE", "_", "L")
         p("GET_BIT", "1", "DOUBLE_CARRY", "_", "L")
-        p("GET_BIT", "E", "REMOVE_OUT", "_", "L")
-        p.find("REMOVE_OUT", OUT, "_", "L", "HALT", "_", "L")
+        p("GET_BIT", EOI, "REMOVE_SOO", "_", "L")
+        p.find("REMOVE_SOO", SOO, "_", "L", "HALT", "_", "L")
 
-        p.find("DOUBLE", OUT, {*digits, "_"}, "L", "DO_DOUBLE", OUT, "L")
-        p.find("DOUBLE_CARRY", OUT, {*digits, "_"}, "L", "DO_DOUBLE_CARRY", OUT, "L")
+        p.find("DOUBLE", SOO, {*BITS, "_"}, "L", "DO_DOUBLE", SOO, "L")
+        p.find("DOUBLE_CARRY", SOO, {*BITS, "_"}, "L", "DO_DOUBLE_CARRY", SOO, "L")
 
         for d in digits:
-            if int(d) * 2 < 10:
-                assert int(d) * 2 + 1 < 10
-                p("DO_DOUBLE", d, "DO_DOUBLE", str((int(d) * 2) % 10), "L")
-                p("DO_DOUBLE_CARRY", d, "DO_DOUBLE", str(int(d) * 2 + 1), "L")
-            else:
-                p("DO_DOUBLE", d, "DO_DOUBLE_CARRY", str((int(d) * 2) % 10), "L")
-                p("DO_DOUBLE_CARRY", d, "DO_DOUBLE_CARRY", str((int(d) * 2 + 1) % 10), "L")
+            should_carry = int(d) * 2 + 1 >= 10
+            p("DO_DOUBLE", d, "DO_DOUBLE_CARRY" if should_carry else "DO_DOUBLE", str((int(d) * 2) % 10), "L")
+            p("DO_DOUBLE_CARRY", d, "DO_DOUBLE", str((int(d) * 2) % 10 + 1), "L")
 
             p("DO_DOUBLE", "_", "TO_INPUT", "_", "R")
             p("DO_DOUBLE_CARRY", "_", "TO_INPUT", "1", "R")
