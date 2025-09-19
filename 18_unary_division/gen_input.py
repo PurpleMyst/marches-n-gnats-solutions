c883b82fcf9e8f43aafe85cb80c0cd9a920d0d12
PurpleMyst <PurpleMyst@users.noreply.github.com>
2025-09-19 22:33:02 +0200

    refactor(18): more methodological input.txt

--- 18_unary_division/gen_input.py
+++ 18_unary_division/gen_input.py
@@ -1,13 +1,11 @@
-import random
 from pathlib import Path
 
 def main() -> None:
     with open(Path(__file__).parent / "input.txt", "w", encoding="utf-8") as f:
-        for _ in range(1024):
-            lhs = random.randint(1, 10)
-            rhs = random.randint(1, 10)
-            q, r = divmod(lhs, rhs)
-            f.write(f"{'|' * lhs}÷{'|' * rhs} => {'|' * q},{'|' * r}\n")
+        for lhs in range(1, 10):
+            for rhs in range(1, 10):
+                q, r = divmod(lhs, rhs)
+                f.write(f"{'|' * lhs}÷{'|' * rhs} => {'|' * q},{'|' * r}\n")
 
 
 
