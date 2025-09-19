e600e1c89fc149eb4959b50d9180e6bffae58d6b
PurpleMyst <PurpleMyst@users.noreply.github.com>
2025-09-19 22:05:40 +0200

    feat(17): add 27 rule solution

---
+++ 18_unary_division/minimal_solution.py
@@ -0,0 +1,34 @@
+from utils import SAME, Program
+
+DIV = "÷"
+
+MARK_LHS = "L"
+
+
+def main() -> None:
+    with Program() as p:
+        p("INIT", "|", "ADD_COMMA", SAME, "L")
+        p("ADD_COMMA", "_", "FIND_EOT", ",", "R")
+
+        p.find("FIND_EOT", "_", {"|", ",", DIV, MARK_LHS}, "R", "NEXT_RHS", SAME, "L")
+
+        p("NEXT_RHS", "|", "NEXT_DIV_LHS", "_", "L")
+        p("NEXT_RHS", DIV, "UNMARK_LHS", "|", "L")
+        p.find("NEXT_LHS", "|", MARK_LHS, "L", "FIND_EOT", MARK_LHS, "R")
+        p.find("NEXT_DIV_LHS",DIV, "|", "L", "NEXT_LHS", SAME, "L")
+
+        p("UNMARK_LHS", MARK_LHS, SAME, "|", "L")
+        p("UNMARK_LHS", "|", "ADD_DIV", SAME, "R")
+        p("UNMARK_LHS", ",", "EARLY_FINISH", "|", "R")
+        p("ADD_DIV", "|", "FIND_COMMA", DIV, "L")
+        p.find("FIND_COMMA", ",", "|", "L", "INC_QUOT", SAME, "L")
+        p.find("INC_QUOT", "_", "|", "L", "FIND_EOT", "|", "R")
+        p("NEXT_LHS", ",", "CLEAN", ",", "R")
+        p("CLEAN", MARK_LHS, SAME, "|", "R")
+        p("CLEAN", {DIV, "|"}, SAME, "_", "R")
+        p("CLEAN", "_", "HALT", "_", "L")
+        p("EARLY_FINISH", "|", "CLEAN", ",", "R")
+
+
+if __name__ == "__main__":
+    main()
