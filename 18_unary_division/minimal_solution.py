ddb92f17fc3a2161b72f0596e1899f5150bfffb0
PurpleMyst <PurpleMyst@users.noreply.github.com>
2025-09-21 14:43:14 +0200

    perf(18/min): mark last lhs to reduce 2 states

--- 18_unary_division/minimal_solution.py
+++ 18_unary_division/minimal_solution.py
@@ -3,6 +3,7 @@ from utils import SAME, Program
 DIV = "÷"
 
 MARK_LHS = "L"
+LAST_LHS = "l"
 
 
 def main() -> None:
@@ -11,22 +12,20 @@ def main() -> None:
         p("ADD_COMMA", "_", "FIND_EOT", ",", "R")
 
         p.find("FIND_EOT", "_", {"|", ",", DIV, MARK_LHS}, "R", "NEXT_RHS", SAME, "L")
+        p("FIND_EOT", LAST_LHS, SAME, MARK_LHS, "R")
 
         p("NEXT_RHS", "|", "NEXT_DIV_LHS", "_", "L")
         p("NEXT_RHS", DIV, "UNMARK_LHS", "|", "L")
-        p.find("NEXT_LHS", "|", MARK_LHS, "L", "FIND_EOT", MARK_LHS, "R")
+        p.find("NEXT_LHS", "|", {MARK_LHS, LAST_LHS}, "L", "FIND_EOT", LAST_LHS, "R")
         p.find("NEXT_DIV_LHS",DIV, "|", "L", "NEXT_LHS", SAME, "L")
 
         p("UNMARK_LHS", MARK_LHS, SAME, "|", "L")
-        p("UNMARK_LHS", "|", "ADD_DIV", SAME, "R")
-        p("UNMARK_LHS", ",", "EARLY_FINISH", "|", "R")
-        p("ADD_DIV", "|", "INC_QUOT", DIV, "L")
+        p("UNMARK_LHS", LAST_LHS, "INC_QUOT", DIV, "L")
         p.find("INC_QUOT", "_", {"|", ","}, "L", "FIND_EOT", "|", "R")
         p("NEXT_LHS", ",", "CLEAN", ",", "R")
-        p("CLEAN", MARK_LHS, SAME, "|", "R")
+        p("CLEAN", {MARK_LHS, LAST_LHS}, SAME, "|", "R")
         p("CLEAN", {DIV, "|"}, SAME, "_", "R")
         p("CLEAN", "_", "HALT", "_", "L")
-        p("EARLY_FINISH", "|", "CLEAN", ",", "R")
 
 
 if __name__ == "__main__":
