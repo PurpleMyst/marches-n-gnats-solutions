32aa96e6dd95126afc58861c9a5119d7fa1f3960
PurpleMyst <PurpleMyst@users.noreply.github.com>
2025-09-28 16:56:10 +0200

    perf(18/min): 20 rule sol

--- 18_unary_division/minimal_solution.py
+++ 18_unary_division/minimal_solution.py
@@ -2,31 +2,40 @@ from utils import SAME, Program
 
 DIV = "÷"
 
+LHS = "l"
+RHS = "r"
+
+POPPED_LHS = "L"
+
 
 def main() -> None:
     with Program() as p:
-        p("INIT", "|", "ADD_COMMA", SAME, "L")
-        p("ADD_COMMA", "_", "GOTO_RHS", ",", "R")
+        p("INIT", "|", "CONV_LHS", "|", "R")
+        p("CONV_LHS", "|", SAME, LHS, "R")
+        p("CONV_LHS", DIV, "CONV_RHS", LHS, "R")
+        p("CONV_RHS", "|", SAME, RHS, "R")
+        p("CONV_RHS", "_", "POP_RHS_OR_INC_QUOT", SAME, "L")
+        p("CONV_RHS", POPPED_LHS, SAME, SAME, "R")
 
-        p.find("GOTO_RHS", "_", {"|", ",", DIV}, "R", "NEXT_RHS", SAME, "L")
+        p("POP_RHS_OR_INC_QUOT", RHS, "POP_LHS", "_", "L")
+        p("POP_RHS_OR_INC_QUOT", POPPED_LHS, "POP_RHS_OR_INC_QUOT", "|", "L")
 
-        p("NEXT_RHS", "|", "GOTO_LHS", "_", "L")
-        p("NEXT_RHS", DIV, "INC_QUOT", "|", "L")
-        p.find("NEXT_LHS", "|", {DIV}, "L", "GOTO_RHS", DIV, "R")
-        p.find("GOTO_LHS", DIV, "|", "L", "NEXT_LHS", SAME, "L")
+        p("POP_LHS", POPPED_LHS, SAME, SAME, "L")
+        p("POP_LHS", RHS, SAME, "|", "L")
+        p("POP_LHS", LHS, "CONV_RHS", POPPED_LHS, "R")
 
-        p("INC_QUOT", DIV, SAME, "|", "L")
-        p("INC_QUOT", "|", "INC_QUOT1", "|", "R")
-        p("INC_QUOT", ",", "INC_QUOT1", ",", "R")
-        p("INC_QUOT1", "|", "INC_QUOT2", DIV, "L")
-        p.find("INC_QUOT2", "_", {"|", ","}, "L", "GOTO_RHS", "|", "R")
-        p("NEXT_LHS", ",", "CLEAN_LHS", ",", "R")
+        p("POP_RHS_OR_INC_QUOT", LHS, SAME, SAME, "L")
+        p("POP_RHS_OR_INC_QUOT", POPPED_LHS, SAME, "|", "L")
+        p("POP_RHS_OR_INC_QUOT", "|", SAME, "/", "L")
+        p("POP_RHS_OR_INC_QUOT", "_", "CONV_RHS", "|", "R")
 
-        p("CLEAN_LHS", "_", "CLEAN_RHS", "_", "L")
-        p("CLEAN_LHS", DIV, SAME, "|", "R")
-        p("CLEAN_LHS", "|", "CLEAN_RHS", "|", "L")
-        p("CLEAN_RHS", "|", SAME, "_", "R")
-        p("CLEAN_RHS", "_", "HALT", "_", "R")
+        p("CONV_RHS", "/", SAME, "|", "R")
+        p("CONV_RHS", LHS, SAME, SAME, "R")
+
+        p("POP_LHS", "|", "CLEAN", ",", "R")
+        p("CLEAN", POPPED_LHS, SAME, "|", "R")
+        p("CLEAN", "|", SAME, "_", "R")
+        p("CLEAN", "_", "HALT", SAME, "L")
 
 
 if __name__ == "__main__":
